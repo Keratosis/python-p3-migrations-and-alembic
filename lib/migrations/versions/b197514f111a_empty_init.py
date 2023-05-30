52be287a0c048abb98e450d6dfb9c0d5a5f0c80d
braxtonshaffie <braxtonshaffie@gmail.com>
2023-05-30 23:54:13 +0300

    creating first migration

---
+++ lib/migrations/versions/b197514f111a_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: b197514f111a
+Revises: 
+Create Date: 2023-05-30 23:51:59.553411
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b197514f111a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
